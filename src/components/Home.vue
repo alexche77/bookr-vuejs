ccdfc39b8bdabc0baf21e0a909097deebffe966e
Alexche77 <alexche7717@gmail.com>
2017-07-23 19:36:06 -0600

    Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main" style="overflow: hidden">  
+        <h1>Home Page</h1>
+        <hr>        
+        <listaLibros></listaLibros>  
+  </div>
+</template>
+
+<script>
+import PublicBooks from './PublicBooks.vue'
+export default {  
+  name: 'Home',
+  components:{
+    'listaLibros': PublicBooks
+  },
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+   methods: {
+    login: function () {
+      console.log('Hola');
+    }
+  },
+}
+</script>
+<style>
+  .centrada {
+    display: block;
+    margin-left: auto;
+    margin-right: auto }
+</style>
---
+++ src/components/PublicBooks.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main" style="overflow: hidden">  
+      <h1>Libros públicos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  beforeCreate: function () {
+    console.log('Trayendo los libros publicos de la base de datos');
+  }
+}
+</script>
+<style>
+  .centrada {
+    display: block;
+    margin-left: auto;
+    margin-right: auto }
+</style>
