6e1a8e81524bce3182191e10b5a8bfe3adae8101
Alexche77 <alexche7717@gmail.com>
2017-07-23 19:35:35 -0600

    Avance en la pagina de inicio y agregado vue-resource

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Main from '@/components/Main'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
 export default new Router({
+	mode: 'history',
   routes: [
+  	{
+    	path: '/',
+    	name: 'home',
+    	component: Home
+    },
     {
-      path: '/',
-      name: 'Main',
-      component: Main
-    },    
+      path: '/login',
+      name: 'login',
+      component: Login
+    },        
   ]
 })
